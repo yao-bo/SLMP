2a389f373f0bc4599896c095396dccb7ab7772dd
yaobo <31315211+yao-bo@users.noreply.github.com>
2018-02-23 18:59:17 +0800

    2-23

--- mainview.h
+++ mainview.h
@@ -2,16 +2,19 @@
 #define MAINVIEW_H
 
 #include <QWidget>
+#include <QVector>
 
 class MainView : public QWidget
 {
     Q_OBJECT
 public:
     explicit MainView(QWidget *parent = 0);
+private:
+    QVector<QWidget> _view;
 
 signals:
 
 public slots:
+    void eviewSelect(int id);
 };
-
-#endif // MAINVIEW_H
+#endif // MAINVIEW_
